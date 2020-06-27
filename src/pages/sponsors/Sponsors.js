575b3194253ae86ce4bcffa1de140f5f98175efb
Pierre Poitier <hanofound@hotmail.com>
2020-06-27 14:33:03 +0200

    Update sponsors

--- src/pages/sponsors/Sponsors.js
+++ src/pages/sponsors/Sponsors.js
@@ -11,11 +11,26 @@ class Sponsors extends Component {
       <div>
         <Hero title='Sponsors du hackathon' content='Sans eux, rien ne serait possible !'>
           <div id="sponsors-list">
-              <img className="sponsor-img" src={process.env.REACT_APP_PUBLIC_URL + "/logo.png"} alt="Sponsor test"/>
-              <img className="sponsor-img" src={process.env.REACT_APP_PUBLIC_URL + "/logo.png"} alt="Sponsor test"/>
-              <img className="sponsor-img" src={process.env.REACT_APP_PUBLIC_URL + "/logo.png"} alt="Sponsor test"/>
-              <img className="sponsor-img" src={process.env.REACT_APP_PUBLIC_URL + "/logo.png"} alt="Sponsor test"/>
-              <img className="sponsor-img" src={process.env.REACT_APP_PUBLIC_URL + "/logo.png"} alt="Sponsor test"/>
+              <a href="https://www.aginsurance.be">
+                  <img className="sponsor-img"
+                       src={"https://www.aginsurance.be/_Layouts/15/images/AG.Portal/AGInsurance_logo.svg"}
+                       alt="Sponsor AG Insurance"/>
+              </a>
+              <a href="https://www.mc.be">
+                  <img className="sponsor-img"
+                       src={"https://www.mc.be/system/202006030920/assets/images/CM_Logo-FR.png"}
+                       alt="Sponsor Mutualité Chrétienne"/>
+              </a>
+              <a href="https://www.trakk.be/">
+                  <img className="sponsor-img"
+                       src={process.env.REACT_APP_PUBLIC_URL + "sponsors/trakk.png"}
+                       alt="Sponsor Trakk"/>
+              </a>
+              <a href="https://www.bep.be">
+                  <img className="sponsor-img"
+                       src={"https://www.bep.be/wp-content/themes/syltaen/_2_assets/img/logo_bep.png"}
+                       alt="Sponsor BEP"/>
+              </a>
           </div>
         </Hero>
       </div>
