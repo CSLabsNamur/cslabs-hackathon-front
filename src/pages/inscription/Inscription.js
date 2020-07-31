5d660b3b802ad05f236ba118c9c8b8b1823d4262
Pierre Poitier <hanofound@hotmail.com>
2020-07-31 12:37:41 +0200

    Add inscription form

--- src/pages/inscription/Inscription.js
+++ src/pages/inscription/Inscription.js
@@ -1,65 +1,333 @@
-import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import React, {Component} from 'react';
+import {Link} from "react-router-dom";
+import {Redirect} from "react-router";
 
 import Countdown from "../../components/countdown/countdown";
 import "./Inscription.css";
+import {UserContext} from "../../context/user";
 
 class Inscription extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      nbParticipant: 1
-    };
-  }
-  drawParticipants() {
-    let partipants = [];
-    for (let index = 0; index < this.state.nbParticipant; index++) {
-      partipants.push(
-        <div>
-          <p>
-            Participant n°{index + 1} :
-          </p>
-          <label>
-            Nom:
-          <input type='text' />
-          </label>
-          <label>
-            Prénom:
-          <input type='text' />
-          </label>
-          <label>
-            Email:
-          <input type='text' />
-          </label>
-        </div>
-      );
-      console.log('salut');
+
+    static contextType = UserContext;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+            password_confirm: "",
+            firstName: "",
+            lastName: "",
+            github: "",
+            linkedin: "",
+            accept_rules: false,
+            validation: {
+                email: null,
+                password: null,
+                password_confirm: null,
+                firstName: null,
+                lastName: null,
+                github: null,
+                linkedin: null,
+                accept_rules: null,
+                server: null
+            },
+            redirect_user: false
+        };
+
+        this.on_submit = this.on_submit.bind(this);
     }
-    return partipants;
-  }
-  render() {
-    // return (
-    //   <div>
-    //     <Hero title='Inscriptions' content="S'inscrire à l'hackathon 2020."></Hero>
-    //     <form className='Form'>
-    //       <label>
-    //         Nombre de participants :
-    //         <input type='number' id='nbParticipant' min='1' max='5' onChange={() => this.setState({ nbParticipant: document.getElementById('nbParticipant').value })} />
-    //       </label>
-    //       {this.drawParticipants()}
-    //     </form>
-    //   </div>
-    return (
-      <div className="container">
-        <h1 id="inscription-title">Inscriptions</h1>
-        <div id="inscription-info">
-          <p>Merci de votre intérêt ! Les inscriptions débuteront en <b>Septembre 2020</b>.</p>
-            <p><i>L'inscription demandera une caution de 20€. <Link to="/infos">Plus d'information</Link>.</i></p>
-          <Countdown destination={new Date(2020, 9, 23)} />
-        </div>
-      </div>
-    )
-  }
+
+    validate_form() {
+        let valid = true;
+
+        const validation = {
+            email: null,
+            password: null,
+            password_confirm: null,
+            firstName: null,
+            lastName: null,
+            github: null,
+            linkedin: null,
+            accept_rules: null,
+            server: null
+        }
+
+        const {
+            email,
+            password,
+            password_confirm,
+            firstName,
+            lastName,
+            github,
+            linkedin,
+            accept_rules
+        } = this.state;
+
+        if (email.length < 1) {
+            validation.email = "Il est nécessaire d'entrer votre email.";
+            valid = false;
+        }
+
+        if (password.length < 10 || password.length > 200) {
+            validation.password = "Le mot de passe doit posséder au minimum 10 caractères et maximum 200 caractères.";
+            valid = false;
+        }
+
+        if (password !== password_confirm) {
+            validation.password_confirm = "Les mots de passe doivent être identiques.";
+            valid = false;
+        }
+
+        if (firstName.length < 3 || firstName.length > 35) {
+            validation.firstName = "Le prénom doit avoir minimum 3 caractères et maximum 35 caractères.";
+            valid = false;
+        }
+
+        if (lastName.length < 3 || lastName.length > 35) {
+            validation.lastName = "Le nom doit avoir minimum 3 caractères et maximum 35 caractères.";
+            valid = false;
+        }
+
+        if (github.length > 0) {
+            if (github.length < 3 || github.length > 256) {
+                validation.github = "Le lien github doit avoir minimum 3 caractères et maximum 256 caractères.";
+                valid = false;
+            }
+        }
+
+        if (linkedin.length > 0) {
+            if (linkedin.length < 3 || linkedin.length > 256) {
+                validation.linkedin = "Le lien linkedin doit avoir minimum 3 caractères et maximum 256 caractères.";
+                valid = false;
+            }
+        }
+
+        if (!accept_rules) {
+            validation.accept_rules = `Il est nécessaire d'accepter ces conditions pour 
+                                       poursuivre votre participation au hackathon.`;
+            valid = false;
+        }
+
+        this.setState({validation: validation});
+
+        return valid;
+    }
+
+    async create_user() {
+
+        const data = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            email: this.state.email,
+            password: this.state.password,
+            github: this.state.github.length > 0 ? this.state.github : null,
+            linkedin: this.state.linkedin.length > 0 ? this.state.linkedin : null
+        }
+
+        const response = await fetch(process.env.REACT_APP_API_URL + 'users/add', {
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+            credentials: 'include',
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify(data)
+        });
+
+        if (response.status !== 200) {
+            this.setState({
+                validation: {server: "Des données sont invalides. Impossible de créer le compte."}
+            });
+            return;
+        }
+
+        const body = await response.json();
+        this.context.authenticate(body);
+
+        console.log("Account created.");
+    }
+
+    on_submit(event) {
+        event.preventDefault();
+
+        if (!this.validate_form()) {
+            console.log("Form validation failed.");
+            return;
+        }
+
+        this.create_user()
+            .then(() => {
+                this.setState({redirect_user: true});
+            })
+            .catch(() => {
+                this.setState({validation: {server: "Impossible de joindre l'hôte distant."}});
+            });
+    }
+
+    render_form_validation_error(message) {
+        return message ? (
+            <p className="validation-error">{message}</p>
+        ) : null;
+    }
+
+    render_form() {
+
+        return (
+            <div className="Form">
+
+                {this.render_form_validation_error(this.state.validation.server)}
+
+                <form id="form-inscription" onSubmit={this.on_submit}>
+
+                    <div className="form-control">
+                        <label htmlFor="form-email">
+                            Votre adresse email
+                        </label>
+                        <input type="text" id="form-email" name="form-email"
+                               placeholder="user@example.com..."
+                               className={this.state.validation.email ? "invalid" : ""}
+                               value={this.state.email}
+                               onChange={event => this.setState({email: event.target.value})}/>
+                        {this.render_form_validation_error(this.state.validation.email)}
+                    </div>
+
+                    <div className="form-control">
+                        <label htmlFor="form-password">
+                            Mot de passe
+                        </label>
+                        <input type="password" id="form-password" name="form-password"
+                               placeholder="super password..."
+                               className={this.state.validation.password ? "invalid" : ""}
+                               value={this.state.password}
+                               onChange={event => this.setState({password: event.target.value})}/>
+                        {this.render_form_validation_error(this.state.validation.password)}
+                    </div>
+
+                    <div className="form-control">
+                        <label htmlFor="form-password-confirm">
+                            Mot de passe (Confirmation)
+                        </label>
+                        <input type="password" id="form-password-confirm" name="form-password-confirm"
+                               placeholder="super password..."
+                               className={this.state.validation.password_confirm ? "invalid" : ""}
+                               value={this.state.password_confirm}
+                               onChange={event => this.setState({password_confirm: event.target.value})}/>
+                        {this.render_form_validation_error(this.state.validation.password_confirm)}
+                    </div>
+
+                    <div className="form-control">
+                        <label htmlFor="form-firstname">
+                            Votre prénom
+                        </label>
+                        <input type="text" id="form-firstname" name="form-firstname"
+                               placeholder="Entrez votre prénom..."
+                               className={this.state.validation.firstName ? "invalid" : ""}
+                               value={this.state.firstName}
+                               onChange={event => this.setState({firstName: event.target.value})}/>
+                        {this.render_form_validation_error(this.state.validation.firstName)}
+                    </div>
+
+                    <div className="form-control">
+                        <label htmlFor="form-name">
+                            Votre nom
+                        </label>
+                        <input type="text" id="form-name" name="form-name"
+                               placeholder="Entrez votre nom..."
+                               className={this.state.validation.lastName ? "invalid" : ""}
+                               value={this.state.lastName}
+                               onChange={event => this.setState({lastName: event.target.value})}/>
+                        {this.render_form_validation_error(this.state.validation.lastName)}
+                    </div>
+
+                    <fieldset>
+                        <legend>Informations complémentaires</legend>
+
+                        <div className="form-control">
+                            <label htmlFor="form-github">
+                                Votre compte github (optionnel)
+                            </label>
+                            <input type="text" id="form-github" name="form-github"
+                                   placeholder="Lien vers votre github..."
+                                   className={this.state.validation.github ? "invalid" : ""}
+                                   value={this.state.github}
+                                   onChange={event => this.setState({github: event.target.value})}/>
+                            {this.render_form_validation_error(this.state.validation.github)}
+                        </div>
+
+                        <div className="form-control">
+                            <label htmlFor="form-linkedin">
+                                Votre compte LinkedIn (optionnel)
+                            </label>
+                            <input type="text" id="form-linkedin" name="form-linkedin"
+                                   placeholder="Lien vers votre linkedin..."
+                                   className={this.state.validation.linkedin ? "invalid" : ""}
+                                   value={this.state.linkedin}
+                                   onChange={event => this.setState({linkedin: event.target.value})}/>
+                            {this.render_form_validation_error(this.state.validation.linkedin)}
+                        </div>
+
+                    </fieldset>
+
+                    <div className="form-control">
+                        <input type="checkbox" id="form-accept-rules" name="form-accept-rules"
+                               value="accept-rules"
+                               checked={this.state.accept_rules}
+                               onChange={event => this.setState({accept_rules: event.target.checked})}/>
+                        <label htmlFor="form-accept-rules">
+                            J'ai pris connaissance des <Link to="/infos">modalités</Link> relatives au hackathon
+                            et notamment de la <strong>caution de 20€</strong>.
+                        </label>
+                        {this.render_form_validation_error(this.state.validation.accept_rules)}
+                    </div>
+
+                    <div className="form-control align-center">
+                        <button type="submit" className="button-primary button-large" id="form-inscription-submit">
+                            M'inscrire
+                        </button>
+                    </div>
+
+                </form>
+
+            </div>
+        );
+
+    }
+
+    render_countdown() {
+        return (
+            <div className="container">
+                <h1 id="inscription-title">Inscriptions</h1>
+                <div id="inscription-info">
+                    <p>Merci de votre intérêt ! Les inscriptions débuteront en <b>Septembre 2020</b>.</p>
+                    <p><i>La participation au hackathon demande <strong>une caution de 20€.</strong></i></p>
+                    <p><Link to="/infos">Plus d'informations.</Link></p>
+                    <Countdown destination={new Date(2020, 9, 23)}/>
+                </div>
+            </div>
+        );
+    }
+
+    render() {
+
+        if (this.state.redirect_user) {
+            return (<Redirect to="/team"/>);
+        }
+
+        return (
+            <div className="container inscription-area">
+                <div className="row">
+                    <div className="col col-lg-6">
+                        {this.render_form()}
+                    </div>
+                    <div className="col col-lg-6">
+                        {this.render_countdown()}
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Inscription;
