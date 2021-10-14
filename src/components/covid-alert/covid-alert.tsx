20ca7cd4bdff5eae203a53610ab2b13522775031
Pierre Poitier <hanofound@hotmail.com>
2021-10-14 17:51:31 +0200

    # Change covid alert message

--- src/components/covid-alert/covid-alert.tsx
+++ src/components/covid-alert/covid-alert.tsx
@@ -9,8 +9,8 @@ export class CovidAlert extends React.PureComponent {
           Dû à la pandémie de la Covid-19, la participation à l'événement nécessite :
         </p>
         <ul>
-          <li>Le port du masque dans l'enceinte de l'établissement</li>
           <li>La présentation du Covid Safe Ticket à l'entrée</li>
+          <li>Le port du masque jusqu'à présentation du Covid Safe Ticket</li>
         </ul>
       </div>
     );
