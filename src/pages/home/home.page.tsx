99e615e74141db62e639d64ab2311e3dcb1e8f56
Yannis Van Achter <yannis.van.achter@gmail.com>
2022-09-23 11:17:26 +0200

    Update home page
    Change date + motd

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -2,135 +2,160 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { PageHero } from "../../components/page-hero/page-hero";
 import { SponsorLogo } from "../../components/sponsor-logo/sponsor-logo";
-import {Topic} from '../../components/topic/topic';
-import {RegistrationInfo} from '../../components/inscription-info/registration-info';
+import { Topic } from "../../components/topic/topic";
+import { RegistrationInfo } from "../../components/inscription-info/registration-info";
 
-import './home.page.css';
-import {MainThematic} from '../../components/main-thematic/main-thematic';
+import "./home.page.css";
+import { MainThematic } from "../../components/main-thematic/main-thematic";
 
 export class HomePage extends React.PureComponent {
-
   private images = [
     {
       src: process.env.PUBLIC_URL + "/images/cslabs_junior.jpg",
-      alt: "CSLabs Junior Entreprise"
+      alt: "CSLabs Junior Entreprise",
     },
     {
-      src: process.env.PUBLIC_URL + "/images/old_hands.jpg",
-      alt: "Thème du Hackathon CSLabs"
+      src: process.env.PUBLIC_URL + "/images/tampon-en-attente.jpg",
+      alt: "Thème du Hackathon CSLabs",
     },
     {
       src: process.env.PUBLIC_URL + "/images/edition.jpg",
-      alt: "Troisième Édition CSLabs"
+      alt: "Troisième Édition CSLabs",
     },
     {
       src: process.env.PUBLIC_URL + "/images/hackathon_irl.jpg",
-      alt: "Hackathon Compétition"
+      alt: "Hackathon Compétition",
     },
     {
       src: process.env.PUBLIC_URL + "/images/prix.png",
-      alt: "Récompense"
-    }
+      alt: "Récompense",
+    },
   ];
 
   render() {
     return (
       <div id="home-page">
-
         <PageHero
-          title="Le bien vieillir"
+          title="Le Handicap"
           hasArrow={true}
-          subtitle="Repenser la technologie pour tous"
+          subtitle="Adapter le monde pour tous"
           disclaimer={
             <div>
-              <p><strong>Du 18 au 20 février à l'UNamur</strong></p>
-              <p>Le Hackathon CSLabs 2021 est organisé dans le cadre du 50e anniversaire de la Faculté d'Informatique</p>
+              <p>
+                <strong>Du 17 au 19 février à l'UNamur</strong>
+              </p>
+              <p>
+                Le Hackathon CSLabs 2023 est organisé dans le cadre du <strong> 5e anniversaire de l'ASBL CSLabs</strong>
+              </p>
               <Link to="/sponsors">
-                  <SponsorLogo  title="Université de Namur"
-                                uri="/sponsors/logo_facinfo.png" />
+                <SponsorLogo
+                  title="Université de Namur"
+                  uri="/sponsors/logo_facinfo.png"
+                />
               </Link>
               <Link to="/sponsors">
-                  <SponsorLogo  title="50 ans de l'université"
-                                uri="/sponsors/logo_50ans_petit.png" />
+                <SponsorLogo
+                  title="50 ans de l'université"
+                  uri="/sponsors/logo_50ans_petit.png"
+                />
               </Link>
-            </div>}>
-        </PageHero>
+            </div>
+          }
+        ></PageHero>
 
         {/*<MainCarousel />*/}
 
-        <Topic img={this.images[0]} direction='right'>
+        <Topic img={this.images[0]} direction="right">
           <h2>CSLabs</h2>
           <p>
-            CSLabs est une junior entreprise de la région de Namur.
-            Spécialisée dans l'informatique, elle invite ses membres à
-            participer à des projets, des formations et des activités dans le
-            domaine technologique et informatique.
+            CSLabs est une junior entreprise de la région de Namur. Spécialisée
+            dans l'informatique, elle invite ses membres à participer à des
+            projets, des formations et des activités dans le domaine
+            technologique et informatique.
+          </p>
+          <strong>Notre mission:</strong>
+          <p>
+            « Offrir l’opportunité aux étudiants d’entreprendre<br></br> dans le
+            domaine de l’informatique »
           </p>
         </Topic>
 
-        <Topic img={this.images[1]} direction='left'>
+        <Topic img={this.images[1]} direction="left">
           <h2>Le thème</h2>
           <p>
-            Nos ainés sont quotidiennement confrontés à la technologie qui
-            évolue à une vitesse prodigieuse. Comment peuvent-ils
-            l'appréhender ? La comprendre ? L'utiliser ?
+            Dans notre société, il y a encore une forte discrimination envers
+            des personnes ayant un handicap. Aidons-les à trouver un travail qui
+            leur convient et rendre leurs inclusions plus aisées.
           </p>
           <p>
             Trouvez des solutions innovantes face à des problèmes réels.
             Développez de nouveaux concepts afin de rendre leur vie meilleure.
           </p>
-          <p>Retrouvez <a href="#main-thematic" className="topic-link on-green">la thématique</a> plus en détail !</p>
+          <p>
+            Retrouvez{" "}
+            <a href="#main-thematic" className="topic-link on-green">
+              la thématique
+            </a>{" "}
+            plus en détail !
+          </p>
         </Topic>
 
         <Topic img={this.images[4]} direction="right">
           <h2>Gagner un prix</h2>
           <p>
-            Envie de remporter un de nos prix ? Un jury et le public désigneront trois vainqueurs !
+            Envie de remporter un de nos prix ? Un jury et le public désigneront
+            trois vainqueurs !
           </p>
           <p>
-            Le <strong>prix de l'idée</strong> est attribué par le jury à la meilleure idée de projet !
+            Le <strong>prix de l'idée</strong> est attribué par le jury à la
+            meilleure idée de projet !
           </p>
           <p>
-            Le <strong>prix de la technique</strong> est, lui aussi par le jury, attribué à la meilleure
-            réalisation technique !
+            Le <strong>prix de la technique</strong> est, lui aussi par le jury,
+            attribué à la meilleure réalisation technique !
           </p>
           <p>
-            Le <strong>prix du public</strong> est attribué au projet qui aura séduit le plus d'équipes !
+            Le <strong>prix du public</strong> est attribué au projet qui aura
+            séduit le plus d'équipes !
           </p>
         </Topic>
 
         <Topic img={this.images[2]} direction="left">
-          <h2>Troisième édition</h2>
+          <h2>Quatrième édition</h2>
           <p>
-            Nous en sommes à notre troisième édition du Hackathon CSLabs !
-            Nous avons hâte de poursuivre l'aventure avec vous !
+            Nous en sommes à notre quatrième édition du Hackathon CSLabs ! Nous
+            avons hâte de poursuivre l'aventure avec vous !
           </p>
           <p>
-            Cette édition est celle de la réalisation !
-            La concrétisation de vos projets nous importe dans la mesure où
-            il est envisageable <Link to="/plus-loin" className="topic-link on-green">d'aller plus loin</Link> et donc d'apporter votre contribution à l'amélioration du Bien Vieillir !
+            Cette édition est celle de la réalisation ! La concrétisation de vos
+            projets nous importe dans la mesure où il est envisageable{" "}
+            <Link to="/plus-loin" className="topic-link on-green">
+              d'aller plus loin
+            </Link>{" "}
+            et donc d'apporter votre contribution à l'amélioration du Bien
+            Vieillir !
           </p>
         </Topic>
 
-        <Topic img={this.images[3]} direction='right'>
-          <h2>
-            Hackathon
-          </h2>
+        <Topic img={this.images[3]} direction="right">
+          <h2>Hackathon</h2>
           <p>
             Un Hackathon est un moment de courte durée où des équipes
-            réfléchissent à une <a href="#main-thematic" className="topic-link">thématique particulière</a>. Ils tentent d'y
-            apporter des solutions en développant des projets innovants.
-            À la fin de l'évènement, un jury détermine quels sont les projets
-            qui ont retenu leur attention et qui ont donc gagné la compétition.
+            réfléchissent à une{" "}
+            <a href="#main-thematic" className="topic-link">
+              thématique particulière
+            </a>
+            . Ils tentent d'y apporter des solutions en développant des projets
+            innovants. À la fin de l'évènement, un jury détermine quels sont les
+            projets qui ont retenu leur attention et qui ont donc gagné la
+            compétition.
           </p>
         </Topic>
 
         <RegistrationInfo />
 
         <MainThematic />
-
-      </div >
+      </div>
     );
   }
-
 }
