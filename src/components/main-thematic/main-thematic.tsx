c074f1a30715f6bf2f2b271968818825f5db21bc
Ugho Raheri <ugho.raherisoanjato@student.unamur.be>
2022-10-16 12:32:47 +0200

    updated main thematic

--- src/components/main-thematic/main-thematic.tsx
+++ src/components/main-thematic/main-thematic.tsx
@@ -12,45 +12,22 @@ export class MainThematic extends React.PureComponent {
 
           <div className="main-thematic__text">
             <p>
-              L’accès à un emploi, la possibilité de pratiquer le sport de ses rêves, la capacité de vivre
-              de manière autonome, … autant de préoccupations essentielles pour les personnes en situation
-              d’handicap. Mais, grâce à la technologie, les individus concernés peuvent bénéficier de solutions
-              adaptées permettant de surmonter les difficultés. Simples ou élaborées, conventionnelles ou innovantes,
-              créées par des associations ou des start-ups, ces réalisations techniques apportent une aide concrète.
-              Handicap et technologie : Adaptons le monde pour tous !, découvrons cette thématique plus en profondeur.
+              La possibilité de pratiquer le sport de ses rêves, la capacité de vivre de manière autonome,
+              l’accessibilté à un emploi, … autant de préoccupations essentielles pour les personnes en situation
+              d’handicap. Mais, grâce à la technologie, les individus concernés peuvent bénéficier de solutions adaptées
+              permettant de surmonter les difficultés. Simples ou élaborées, conventionnelles ou innovantes, créées par
+              des associations ou des start-ups, ces réalisations techniques apportent une aide concrète.
+              <strong> Handicap et technologie : Adaptons le monde pour tous !</strong>, découvrons cette thématique.
             </p>
+          </div>
 
-          <h5 className="tx-centered">Qu’entend-on par handicap ?</h5>
+          <br/>
+          <h5>Qu’entend-on par handicap ?</h5>
 
           <p>
             Le terme "handicap" réfère à la limitation des possibilités d'interaction d'un individu avec son environnement,
-            menant à des difficultés. On distingue différents types d’handicap :
-          </p>
-        </div>
-
-          <ul>
-            <li>
-              <h6>Handicap physique</h6>
-              <p>Recouvre l’ensemble des troubles pouvant entraîner une atteinte partielle ou totale de la motricité</p>
-            </li>
-            <li>
-              <h6>Handicap sensoriel</h6>
-              <p>Regroupe les difficultés liées aux organes sensoriels, avec notamment le handicap visuel et auditif</p>
-            </li>
-            <li>
-              <h6>Handicap mental</h6>
-              <p>Concerne une difficulté à comprendre ou encore une limitation dans la rapidité des fonctions
-                de la compréhension</p>
-            </li>
-            <li>
-              <h6>Handicap cognitif</h6>
-              <p>Regroupe des difficultés en termes de facultés d'apprentissage ou de perception de l'environnement</p>
-            </li>
-          </ul>
-
-          <p>
-            Le handicap est souvent défini comme une inégalité de moyens, et faute de moyen,
-            il peut devenir un facteur d’exclusion.
+            menant à des difficultés. On distingue différents types d’handicap : physique, sensoriel, mental ou cognitif.
+            Le handicap est souvent défini comme une inégalité de moyens, et faute de moyen, il peut devenir un facteur d’exclusion.
           </p>
 
           <br/>
@@ -63,13 +40,10 @@ export class MainThematic extends React.PureComponent {
 
           <h6>Quand la technologie sert à sensibiliser ?</h6>
           <p>
-            La marginalisation des personnes handicapées est un vrai problème. Ce sont, d’abord, les petites
-            différences du quotidien et puis c’est aussi le regard des autres. Sensibiliser à une telle problématique
-            n’est pas chose facile. Pour cela, il faudrait parvenir à se glisser dans la peau d’une personne vivant
-            en situation de handicap, permettant de comprendre ses limitations, la façon de les surmonter, de conserver
-            ou de retrouver son autonomie. La technologie permet tout cela. Citons par exemple les combinaisons de
-            vieillissement, permettant de simuler une perte d’autonomie ou encore les casques de réalité virtuelle
-            permettant de s’immerger dans une situation de handicap.
+            La marginalisation des personnes handicapées est un vrai problème. Ce sont, d’abord, les petites différences
+            du quotidien et puis c’est aussi le regard des autres. Sensibiliser à une telle problématique n’est pas chose facile.
+            Pour cela, il faudrait parvenir à se glisser dans la peau d’une personne vivant en situation de handicap, permettant
+            de comprendre ses limitations, la façon de les surmonter, de conserver ou de retrouver son autonomie. 
           </p>
 
           <h6>La technologie, au service de la qualité de vie ?</h6>
@@ -97,20 +71,32 @@ export class MainThematic extends React.PureComponent {
           <p>
             Et si, ensemble, nous adaptions le monde pour tous ! A vous de jouer : quelles solutions pouvons-nous
             développer pour rendre l’impossible possible pour les personnes en situation d’handicap ? “Malgré la
-            différence, on est capable de faire les mêmes choses… voire même parfois mieux !” [Flavien Gelly – influencer]. 
+            différence, on est capable de faire les mêmes choses… voire même parfois mieux !” [Flavien Gelly]. 
           </p>
 
           <br/>
           <h5>Références :</h5>
 
-          <p>
-            •	<a href="https://fr.wikipedia.org/wiki/Handicap">https://fr.wikipedia.org/wiki/Handicap</a><br/>
-            •	<a href="https://www.boutique-box-internet.fr/actualites/handicap-technologie/">https://www.boutique-box-internet.fr/actualites/handicap-technologie/</a><br/>
-            •	<a href="https://blog.ipedis.com/accessibilite-handicapes-nouvelles-technologie">https://blog.ipedis.com/accessibilite-handicapes-nouvelles-technologie</a><br/>
-            •	<a href="https://www.forbes.fr/technologie/la-technologie-au-service-des-personnes-en-situation-de-handicap/">https://www.forbes.fr/technologie/la-technologie-au-service-des-personnes-en-situation-de-handicap/</a><br/>
-            •	<a href="https://www.lesechos.fr/thema/mobilites-innovations/mobilites-le-handicap-un-moteur-dinnovations-sous-estime-1038277">https://www.lesechos.fr/thema/mobilites-innovations/mobilites-le-handicap-un-moteur-dinnovations-sous-estime-1038277</a><br/>
-            •	<a href="https://www.nouvelobs.com/societe/20190529.AFP7500/handicap-l-innovation-technologique-pour-relever-le-defi-de-l-accessibilite.html">https://www.nouvelobs.com/societe/20190529.AFP7500/handicap-l-innovation-technologique-pour-relever-le-defi-de-l-accessibilite.html</a>
-          </p>
+          <ul>
+            <li>
+              <a href="https://fr.wikipedia.org/wiki/Handicap">https://fr.wikipedia.org/wiki/Handicap</a>
+            </li>
+            <li>
+              <a href="https://www.boutique-box-internet.fr/actualites/handicap-technologie/">https://www.boutique-box-internet.fr/actualites/handicap-technologie/</a>
+            </li>
+            <li>
+              <a href="https://blog.ipedis.com/accessibilite-handicapes-nouvelles-technologie">https://blog.ipedis.com/accessibilite-handicapes-nouvelles-technologie</a>
+            </li>
+            <li>
+              <a href="https://www.forbes.fr/technologie/la-technologie-au-service-des-personnes-en-situation-de-handicap/">https://www.forbes.fr/technologie/la-technologie-au-service-des-personnes-en-situation-de-handicap/</a>
+            </li>
+            <li>
+              <a href="https://www.lesechos.fr/thema/mobilites-innovations/mobilites-le-handicap-un-moteur-dinnovations-sous-estime-1038277">https://www.lesechos.fr/thema/mobilites-innovations/mobilites-le-handicap-un-moteur-dinnovations-sous-estime-1038277</a>
+            </li>
+            <li>
+              <a href="https://www.nouvelobs.com/societe/20190529.AFP7500/handicap-l-innovation-technologique-pour-relever-le-defi-de-l-accessibilite.html">https://www.nouvelobs.com/societe/20190529.AFP7500/handicap-l-innovation-technologique-pour-relever-le-defi-de-l-accessibilite.html</a>
+            </li>
+          </ul>
 
         </div>
       </div>
