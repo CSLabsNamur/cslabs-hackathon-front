98d4fbfd7da32e0e603501e5305dd5c01a217ef1
Pierre Poitier <hanofound@hotmail.com>
2023-02-16 22:17:25 +0100

    Forgot the title

--- src/pages/draw-board/draw-board.page.tsx
+++ src/pages/draw-board/draw-board.page.tsx
@@ -7,6 +7,7 @@ export class DrawBoardPage extends React.PureComponent {
     return (
       <iframe
         id="draw-board"
+        title="Draw-board hackathon 2023"
         src="https://www.web-whiteboard.io/?id=137638&embedToken=HLl+cWEQ24jrbYB5y7P7RKOWonscMYBg"
         allow="camera; encrypted-media; microphone; midi;"
         allowFullScreen={true}>
