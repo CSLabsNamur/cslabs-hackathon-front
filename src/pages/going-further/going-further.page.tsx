190e9846d690922771e4a6590c0872f0085178ac
Yannis Van Achter <91072987+YannisVanAchter@users.noreply.github.com>
2023-11-07 15:29:08 +0100

    Fix bug + add Linkube

--- src/pages/going-further/going-further.page.tsx
+++ src/pages/going-further/going-further.page.tsx
@@ -16,7 +16,7 @@ export class GoingFurtherPage extends React.PureComponent {
           hasArrow
         >
           <p className="further-page__intro">
-            Ton projet peut changer les choses. N'hésite pas à consulter nos sponsors pour lui construire un
+            Ton projet peut changer les choses. <br />   N'hésite pas à consulter nos sponsors pour lui construire un
             avenir ! <Link to='/space-invader'>🎮</Link>
           </p>
         </PageHero>
@@ -25,7 +25,7 @@ export class GoingFurtherPage extends React.PureComponent {
 
           <div className="further-page__content">
 
-            {/* <div className="further-page__sponsor">
+            <div className="further-page__sponsor">
               <h1 className="tx-centered">Le TRAKK | LinKube</h1>
 
               <h5>Toutes tes idées ont un avenir !</h5>
@@ -58,7 +58,7 @@ export class GoingFurtherPage extends React.PureComponent {
               </ul>
 
               <p>Un chemin de possibles s’offre à toi pour concrétiser tes beaux projets. On en discute ?</p>
-            </div> */}
+            </div>
 
             {/* <div className="further-page__sponsor">
               <h1 className={"tx-centered"}>La Mutualité Chrétienne</h1>
