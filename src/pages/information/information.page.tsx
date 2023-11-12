181572feb3b2cc1013fcda8f1d015621714be1cf
Pluvic <victor.pluvinage@student.unamur.be>
2023-11-12 14:52:16 +0100

    Update Informations
    Mise à jour des infos au sujet des lits de camps

--- src/pages/information/information.page.tsx
+++ src/pages/information/information.page.tsx
@@ -39,9 +39,10 @@ export class InformationPage extends React.PureComponent {
         </InfoItem>
 
         <InfoItem title="Commodités" icon="/infos/commodites.svg">
-          <p>Repos: <b>Salle avec des lits prévue</b></p>
+          <p>Repos: <b>Salle avec des lits prévue*</b></p>
           <p>Repas: <b>Repas fournis</b> <Link to='/pizza-clicker'>🍕</Link></p>
           <p>Sanitaires: <b>Installations de l'Université</b></p>
+          <p>* Des lits de camp seront mis à disposition mais il est vivement recommandé d'apporter votre propre lit de camp ou matelas gonflable</p>
         </InfoItem>
 
         <InfoItem title="Inscription" icon="/infos/register.svg">
