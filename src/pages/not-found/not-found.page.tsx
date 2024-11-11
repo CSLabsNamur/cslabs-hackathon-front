3ad387c22fbedd26b5d4cf2ff9071eb1e72cba3c
Martin <jacobmartin03@hotmail.com>
2024-11-11 18:49:33 +0100

    fix: Remove wrong expression in the 404 page

--- src/pages/not-found/not-found.page.tsx
+++ src/pages/not-found/not-found.page.tsx
@@ -7,14 +7,12 @@ export class NotFoundPage extends React.PureComponent {
   render() {
     return (
       <div id="not-found-page">
-        <h2>Cette page n'existe pas !? Que nenni ?!</h2>
+        <h2>Cette page n'existe pas...</h2>
 
         <p>Il se pourrait que vous soyez perdu l'ami...</p>
 
         <img src="/images/hmmm.gif" alt="Emote qui tourne"/>
-
       </div>
     );
   }
-
 }
