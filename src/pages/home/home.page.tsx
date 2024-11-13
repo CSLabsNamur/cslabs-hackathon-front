c38dae632546b9ff0762950c0a1a84d60ffaab5e
Martin <jacobmartin03@hotmail.com>
2024-11-13 16:59:27 +0100

    chore: Move CSLabs's text down

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -45,13 +45,72 @@ export class HomePage extends React.PureComponent {
             <div className="on-green">
               <p><strong>Du {import.meta.env.VITE_DATE_EVENT} à l'Université de Namur</strong></p>
               <div className="home-page__logo_list">
+                <SponsorLogo href="https://cslabs.be/" newTab title="CSLabs"
+                             uri="/images/logo/CSLabs.svg"/>
                 <SponsorLogo to="/sponsors" title="Université de Namur"
                              uri="/sponsors/unamur.png"/>
-                <SponsorLogo href="https://cslabs.be/" newTab title="CSLabs"
-                             uri="/images/logo/CSLabs.svg"/></div>
+              </div>
             </div>
           }>
         </PageHero>
+
+        <Topic img={this.images[3]} direction="right">
+          <h2 className="on-white">
+            Hackathon
+          </h2>
+          <p className="on-white">
+            Un Hackathon est un moment de courte durée où des équipes
+            réfléchissent à une <a href="#main-thematic" className="topic-link">thématique particulière</a>.
+            Elles
+            tentent d'y apporter des solutions en développant des projets innovants.
+            À la fin de l'évènement, un jury détermine quels sont les projets
+            qui ont retenu leur attention et qui ont donc gagné la compétition.
+          </p>
+        </Topic>
+
+        <Topic img={this.images[2]} direction="left">
+          <h2 className="on-green">Sixième édition</h2>
+          <p className="on-green">
+            Nous sommes ravis de vous proposer une nouvelle édition de notre Hackathon CSLabs !
+            Cet événement est multidisciplinaire et ouvert à tous et à toutes. Aucune compétence spécifique n'est
+            requise pour notre Hackathon, ce qui permet à chacun d'apporter sa créativité et ses idées.
+          </p>
+          <p className="on-green">
+            La concrétisation de vos projets nous importe, dans la mesure où il est
+            envisageable <Link to="/plus-loin" className="topic-link on-green">d'aller plus loin</Link>, et donc
+            d'apporter une contribution réelle !
+          </p>
+        </Topic>
+
+        <Topic img={this.images[1]} direction="right">
+          <h2 className="on-white">Le thème</h2>
+          <p className="on-white">
+            Trouvez des solutions innovantes pour faire face à des problèmes réels.
+            Développez de nouveaux concepts afin de rendre leur vie meilleure.
+          </p>
+          <p className="on-white">Comment l'informatique peut-elle contribuer à la promotion et à la préservation du
+            patrimoine culturel ?</p>
+          <p className="on-white">Retrouvez <a href="#main-thematic" className="topic-link">la
+            thématique</a> plus en détail !</p>
+        </Topic>
+
+        <Topic img={this.images[4]} direction="left">
+          <h2 className="on-green">Gagner un prix</h2>
+          <p className="on-green">
+            Envie de remporter un de nos prix ?
+          </p>
+          <p className="on-green">
+            Le <strong>prix de l'innovation</strong> est décerné par le jury au projet le plus novateur, celui qui les a
+            le plus convaincus lors de la présentation finale.
+          </p>
+          <p className="on-green">
+            Le <strong>prix de la technique</strong> est, lui aussi par le jury, attribué à la réalisation technique la
+            plus aboutie.
+          </p>
+          <p className="on-green">
+            Le <strong>prix du public</strong> sera attribué au projet préféré du public.
+          </p>
+        </Topic>
 
         <Topic img={this.images[0]} direction="right">
           <h2 className="on-white">CSLabs</h2>
@@ -64,64 +123,6 @@ export class HomePage extends React.PureComponent {
           <p className="on-white">
             <strong>Notre mission : </strong>"Offrir l'opportunité aux étudiants d'entreprendre dans le domaine de
             l'informatique"
-          </p>
-        </Topic>
-
-        <Topic img={this.images[3]} direction="left">
-          <h2 className="on-green">
-            Hackathon
-          </h2>
-          <p className="on-green">
-            Un Hackathon est un moment de courte durée où des équipes
-            réfléchissent à une <a href="#main-thematic" className="topic-link on-green">thématique particulière</a>.
-            Elles
-            tentent d'y apporter des solutions en développant des projets innovants.
-            À la fin de l'évènement, un jury détermine quels sont les projets
-            qui ont retenu leur attention et qui ont donc gagné la compétition.
-          </p>
-        </Topic>
-
-        <Topic img={this.images[2]} direction="right">
-          <h2 className="on-white">Sixième édition</h2>
-          <p className="on-white">
-            Nous sommes ravis de vous proposer une nouvelle édition de notre Hackathon CSLabs !
-            Cet événement est multidisciplinaire et ouvert à tous et à toutes. Aucune compétence spécifique n'est
-            requise pour notre Hackathon, ce qui permet à chacun d'apporter sa créativité et ses idées.
-          </p>
-          <p className="on-white">
-            La concrétisation de vos projets nous importe, dans la mesure où il est
-            envisageable <Link to="/plus-loin" className="topic-link">d'aller plus loin</Link>, et donc
-            d'apporter une contribution réelle !
-          </p>
-        </Topic>
-
-        <Topic img={this.images[1]} direction="left">
-          <h2 className="on-green">Le thème</h2>
-          <p className="on-green">
-            Trouvez des solutions innovantes pour faire face à des problèmes réels.
-            Développez de nouveaux concepts afin de rendre leur vie meilleure.
-          </p>
-          <p className="on-green">Comment l'informatique peut-elle contribuer à la promotion et à la préservation du
-            patrimoine culturel ?</p>
-          <p className="on-green">Retrouvez <a href="#main-thematic" className="topic-link on-green">la
-            thématique</a> plus en détail !</p>
-        </Topic>
-
-        <Topic img={this.images[4]} direction="right">
-          <h2 className="on-white">Gagner un prix</h2>
-          <p className="on-white">
-            Envie de remporter un de nos prix ?
-          </p>
-          <p className="on-white">
-            Le <strong>prix de l'innovation</strong> est décerné par le jury au projet le plus novateur, celui qui les a
-            le plus convaincus lors de la présentation finale.
-          </p>
-          <p className="on-white">
-            Le <strong>prix de la technique</strong> est, lui aussi par le jury, attribué à la réalisation technique la
-            plus aboutie.
-          </p>
-          <p className="on-white">
-            Le <strong>prix du public</strong> sera attribué au projet préféré du public.
           </p>
         </Topic>
 
