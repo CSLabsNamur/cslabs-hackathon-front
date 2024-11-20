69aafac65bac3a798c0859acab9b8c0073836dd3
Martin <jacobmartin03@hotmail.com>
2024-11-20 16:13:37 +0100

    feat: Add TOS link in the footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -10,6 +10,10 @@ export class Footer extends React.PureComponent {
           <div className="footer__content__left">
             <Link to="https://cslabs.be" className="on-green">❤️ CSLabs</Link>
           </div>
+          <div className="footer__content__center">
+            <a href={"/documents/termes_et_conditions.pdf"} rel="noopener noreferrer" target="_blank"
+               className="on-green">Termes et conditions</a>
+          </div>
           <div className="footer__content__right">
             <Link to="/cookie-policy" className="on-green">Politique de cookies</Link>
           </div>
